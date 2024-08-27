692f9a2d44fb6a0fc75490a2d8797530ee22a583
igel7 <168667349+igel7@users.noreply.github.com>
2024-08-27 10:49:51 +0900

    main.py の更新

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ Created on Wed May  1 01:43:48 2024
 @author: ryasu
 """
 
-# メインプログラム
+# Load packages
 print('Loading Packages...')
 from data_loader import DataLoader
 from map_creator import MapCreator
 from ui import UI
 
-# # デバッグ用
+# # for debug
 # import os
 # os.chdir('C:\\Users\\ryasu\\Desktop\\WPy64-31050\\notebooks\\folium')
 
-# データファイルのパス
+# path to data files
 geojson_path = "data/gdf.geojson"
 geojson_ken_path = "data/gdf_ken.geojson"
 railroad_path = "data/tetsudo/N02-22_RailroadSection.shp"
@@ -23,10 +23,10 @@ highway_path = "data/douro/N06-22_HighwaySection.shp"
 convenience_path = "data/df_convenience.feather"
 post_office_path = "data/df_post_office.feather"
 
-# data_loaderのインスタンスを作成
+# make instance of data_loader
 data_loader = DataLoader(geojson_path, geojson_ken_path, railroad_path, highway_path, convenience_path, post_office_path)
 
-# 必要なデータの読み込み
+# Load data
 print('Loading geoJSON data...')
 geojson_data = data_loader.load_geojson()
 geojson_data_ken = data_loader.load_geojson_ken()
@@ -37,10 +37,10 @@ print('Loading feather data...')
 convenience_data = data_loader.load_convenience()
 post_office_data = data_loader.load_post_office()
 
-# MapCreatorのインスタンスを作成
+# make instance of MapCreator
 map_creator = MapCreator(geojson_data, geojson_data_ken, railroad_data, highway_data, convenience_data, post_office_data)
 
 
-# UIインスタンスの作成とアプリケーションの実行
+# Make instance for UI and start the app
 ui = UI(map_creator)
 ui.run()
